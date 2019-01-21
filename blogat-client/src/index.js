8d598c2849c1cd8c9c8eb8da9812c484edd54ba8
lshaw212 <lshaw212@gmail.com>
2019-01-21 16:32:59 +0000

    serviceworker unregistered

--- blogat-client/src/index.js
+++ blogat-client/src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
+import { unregister } from './registerServiceWorker';
+
+unregister();
 
 ReactDOM.render(<App/>, document.getElementById('root'));
 registerServiceWorker();
