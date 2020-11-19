f4894c5a0ada45d8c1ff9e0875e17734aa23ac45
lshaw212 <lshaw212@gmail.com>
2020-11-19 21:12:06 +0000

    Updated Database

--- blogat-server/models/index.js
+++ blogat-server/models/index.js
@@ -1,9 +1,25 @@
+const username = process.env.data_username;
+const password = process.env.data_password
+const CONNECTION_URL = `mongodb+srv://${username}:${password}@blogat.ugjbd.mongodb.net/blogat?retryWrites=true&w=majority`;
+
+
+const options = {
+  reconnectTries: Number.MAX_VALUE,
+  poolSize: 10,
+  autoIndex: false
+}
 const mongoose = require("mongoose");
 mongoose.set("debug", true);
 mongoose.Promise = Promise;
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/blogat", {
-  keepAlive: true
-});
+
+mongoose.connect(CONNECTION_URL, options).then(
+  () => {
+    console.log("Database connection established!");
+  },
+  err => {
+    console.log("Error connecting to the Database due to:", err);
+  }
+);
 
 module.exports.User = require("./user");
 module.exports.Blog = require("./blogs");
